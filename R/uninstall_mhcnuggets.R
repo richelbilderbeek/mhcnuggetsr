34e854dee1f337e01162ac64b7247214cab79271
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-25 13:16:44 +0200

    Can install and uninstall MHCnuggets

--- R/uninstall_mhcnuggets.R
+++ R/uninstall_mhcnuggets.R
@@ -1,9 +1,24 @@
 #' Uninstall the MHCnuggets Python package.
 #' @author Richèl J.C. Bilderbeek
 #' @export
-uninstall_mhcnuggets <- function() {
-  python <- reticulate:::.globals$py_config$python
-  reticulate:::pip_uninstall(python = python, package = "mhcnuggets")
+uninstall_mhcnuggets <- function(
+  folder_name = get_default_mhcnuggets_folder(),
+  mhcnuggets_url = get_mhcnuggets_url()
+) {
+  mhcnuggetsr::check_mhcnuggets_installation(
+    folder_name = folder_name,
+    mhcnuggets_url = mhcnuggets_url
+  )
+
+  # Delete folder
+  mhcnuggets_folder <- file.path(folder_name, basename(mhcnuggets_url))
+  unlink(mhcnuggets_folder, recursive = TRUE, force = TRUE)
+  testthat::expect_true(!dir.exists(mhcnuggets_folder))
+
+  if (1 == 2) {
+    python <- reticulate:::.globals$py_config$python
+    reticulate:::pip_uninstall(python = python, package = "mhcnuggets")
+  }
 
   if (mhcnuggetsr::is_mhcnuggets_installed()) {
     warning("The Python module 'mhcnuggets' is not uninstalled")
