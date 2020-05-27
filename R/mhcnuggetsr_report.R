6b29473982a1ec82bc93a968aab51fef4f6712d4
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-27 07:15:01 +0200

    Improve 'mhcnuggetsr_report'

--- R/mhcnuggetsr_report.R
+++ R/mhcnuggetsr_report.R
@@ -13,6 +13,7 @@ mhcnuggetsr_report <- function() {
   kat(paste0("OS: ", rappdirs::app_dir()$os))
   kat(paste0("Python available: ", reticulate::py_available()))
   kat(paste0("Python location: ", reticulate::py_config()$python))
+  print(reticulate::py_config())
   kat(paste0("pip version: ", mhcnuggetsr::get_pip_version()))
   kat("**************")
   kat("* MHCnuggets *")
