35cdb1279b0c7df5b69587912aebc42f5f31262f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-27 11:57:58 +0200

    Use different name

--- scripts/install_mhcnuggets.sh
+++ scripts/install_mhcnuggets.sh
@@ -10,11 +10,12 @@
 #  ./scripts/install_mhcnuggets.sh
 #
 #
-cat R/install_mhcnuggets.R > install_mhcnuggets.R
-cat R/get_default_mhcnuggets_folder.R >> install_mhcnuggets.R
-cat R/get_mhcnuggets_url.R >> install_mhcnuggets.R
-cat R/is_mhcnuggets_installed.R >> install_mhcnuggets.R
-echo "install_mhcnuggets()" >> install_mhcnuggets.R
-sed -i'.orginal' "s/mhcnuggetsr:://g" install_mhcnuggets.R
-cat install_mhcnuggets.R
-Rscript install_mhcnuggets.R
+cat R/install_mhcnuggets.R > tmp_install_mhcnuggets.R
+cat R/get_default_mhcnuggets_folder.R >> tmp_install_mhcnuggets.R
+cat R/get_mhcnuggets_url.R >> tmp_install_mhcnuggets.R
+cat R/is_mhcnuggets_installed.R >> tmp_install_mhcnuggets.R
+echo "install_mhcnuggets()" >> tmp_install_mhcnuggets.R
+sed -i'.orginal' "s/mhcnuggetsr:://g" tmp_install_mhcnuggets.R
+cat tmp_install_mhcnuggets.R
+Rscript tmp_install_mhcnuggets.R
+rm tmp_install_mhcnuggets.R
