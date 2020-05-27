6dfa18fd2756e7b4512d42bf08de06e5f18750da
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-27 11:02:33 +0200

    More ::: :frown:

--- tests/testthat/test-install_mhcnuggets.R
+++ tests/testthat/test-install_mhcnuggets.R
@@ -2,15 +2,15 @@ test_that("use", {
   if (!is_on_ci()) return()
 
   if (!is_mhcnuggets_installed()) {
-    install_mhcnuggets()
+    mhcnuggetsr:::install_mhcnuggets()
     expect_true(is_mhcnuggets_installed())
     get_mhcnuggets_version()
     mhcnuggetsr:::uninstall_mhcnuggets()
     expect_false(is_mhcnuggets_installed())
   } else {
-    uninstall_mhcnuggets()
+    mhcnuggetsr:::uninstall_mhcnuggets()
     expect_false(is_mhcnuggets_installed())
-    install_mhcnuggets()
+    mhcnuggetsr:::install_mhcnuggets()
     expect_true(is_mhcnuggets_installed())
   }
 })
