cf53b7ba600fcbac3bcca48d4cdded6955a584de
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-27 12:17:24 +0200

    Use 'installl_', due to bug upstream

--- scripts/install_mhcnuggets.sh
+++ scripts/install_mhcnuggets.sh
@@ -14,7 +14,9 @@ cat R/install_mhcnuggets.R > tmp_install_mhcnuggets.R
 cat R/get_default_mhcnuggets_folder.R >> tmp_install_mhcnuggets.R
 cat R/get_mhcnuggets_url.R >> tmp_install_mhcnuggets.R
 cat R/is_mhcnuggets_installed.R >> tmp_install_mhcnuggets.R
-echo "install_mhcnuggets()" >> tmp_install_mhcnuggets.R
+
+# Use triple-l for now
+echo "installl_mhcnuggets()" >> tmp_install_mhcnuggets.R
 sed -i'.orginal' "s/mhcnuggetsr:://g" tmp_install_mhcnuggets.R
 cat tmp_install_mhcnuggets.R
 Rscript tmp_install_mhcnuggets.R
