8f73ddca326a8b1125de63e2b605a051e9388a74
richelbilderbeek <richel@richelbilderbeek.nl>
2020-05-28 07:42:48 +0200

    Predict IC50s of longer peptides

--- R/predict_ic50s.R
+++ R/predict_ic50s.R
@@ -1,5 +1,22 @@
 #' Predict IC50s (in nM) of 15-mers of a peptide
 #' @inheritParams default_params_doc
+#' @examples
+#' peptide <- "AIAACAMLLVCCCCCC"
+#'
+#' df <- predict_ic50s(
+#'   mhc_class = "I",
+#'   peptide = peptide,
+#'   mhc = "HLA-A02:01"
+#' )
+#'
+#' expect_true("peptide" %in% names(df))
+#' expect_true("ic50" %in% names(df))
+#' expect_equal("character", class(df$peptide))
+#' expect_equal("numeric", class(df$ic50))
+#' n_rows_expected <- nchar(peptide) - 15 + 1
+#' expect_equal(n_rows_expected, nrow(df))
+#' @author Richèl J.C. Bilderbeek
+#' @export
 predict_ic50s <- function(
   mhc_class,
   peptide,
