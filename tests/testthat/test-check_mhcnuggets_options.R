c90d06a1b19a730b4e63e836ba2cc2f6010c9e3a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-03 12:23:29 +0200

    Increase codecov

--- tests/testthat/test-check_mhcnuggets_options.R
+++ tests/testthat/test-check_mhcnuggets_options.R
@@ -4,6 +4,7 @@ test_that("use", {
 
   expect_silent(check_mhcnuggets_options(create_test_mhcnuggets_options()))
 
+  # Must be a list
   expect_error(
     check_mhcnuggets_options("nonsense"),
     "'mhcnuggets_options' must be a list"
@@ -14,6 +15,7 @@ test_that("use", {
   expect_error(check_mhcnuggets_options(3.14))
   expect_error(check_mhcnuggets_options(42))
 
+  # Element names
   expect_error(
     check_mhcnuggets_options(list()),
     "'mhcnuggets_options' must have an element named 'mhc_class'"
@@ -24,4 +26,42 @@ test_that("use", {
     )
   )
 
+
+
+  expect_error(
+    check_mhcnuggets_options(
+      create_test_mhcnuggets_options(
+        mhc_class = "nonsense"
+      )
+    ),
+    "'mhc_class' must be either 'I' or 'II'"
+  )
+
+  expect_error(
+    check_mhcnuggets_options(
+      create_test_mhcnuggets_options(
+        mhc = "nonsense"
+      )
+    ),
+    "'mhc' must be a valid MHC haplotype name"
+  )
+
+  expect_error(
+    check_mhcnuggets_options(
+      create_test_mhcnuggets_options(
+        mhc_class = "I",
+        mhc = get_trained_mhc_2_haplotypes()[1]
+      )
+    ),
+    "Must use the same 'mhc_class' as the 'mhc' is from"
+  )
+  expect_error(
+    check_mhcnuggets_options(
+      create_test_mhcnuggets_options(
+        mhc_class = "II",
+        mhc = get_trained_mhc_1_haplotypes()[1]
+      )
+    ),
+    "Must use the same 'mhc_class' as the 'mhc' is from"
+  )
 })
