bd867c7d9c78340ee8afa6064839c53eae082dc8
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-26 11:51:08 +0200

    Get Python package versions only if pip is installed

--- tests/testthat/test-get_python_package_versions.R
+++ tests/testthat/test-get_python_package_versions.R
@@ -1,27 +1,12 @@
 test_that("use", {
-  if (is_on_travis()) {
-    df <- get_python_package_versions()
-    message(df)
-    expect_true(tibble::is_tibble(df))
-    expect_true("package" %in% names(df))
-    expect_true("version" %in% names(df))
-    expect_true(nrow(df) > 0)
-    expect_true(!is.factor(df$package))
-    expect_true(!is.factor(df$version))
-  } else if (is_on_appveyor()) {
-    # Show package versions
-    system2(
-      reticulate::py_config()$python,
-      args = c("-m", "pip", "freeze"),
-      stdout = TRUE
-    )
-    df <- get_python_package_versions()
-    message(df)
-    expect_true(tibble::is_tibble(df))
-    expect_true("package" %in% names(df))
-    expect_false("version" %in% names(df)) # Weird!
-    expect_true(nrow(df) > 0)
-    expect_true(!is.factor(df$package))
-    expect_true(!is.factor(df$version))
-  }
+  if (!is_pip_installed()) return()
+
+  df <- get_python_package_versions()
+  message(df)
+  expect_true(tibble::is_tibble(df))
+  expect_true("package" %in% names(df))
+  expect_true("version" %in% names(df))
+  expect_true(nrow(df) > 0)
+  expect_true(!is.factor(df$package))
+  expect_true(!is.factor(df$version))
 })
