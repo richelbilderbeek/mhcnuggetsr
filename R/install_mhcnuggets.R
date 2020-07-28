13e31ea2ba2e85b3ea93c0301872f9a5d900041d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-28 16:20:56 +0200

    'install_mhcnuggets' checks if pip is installed after installing pip

--- R/install_mhcnuggets.R
+++ R/install_mhcnuggets.R
@@ -47,6 +47,7 @@ install_mhcnuggets <- function(
       "-m", "pip", "install", "--upgrade", "pip", "--quiet"
     )
   )
+  testthat::expect_true(is_pip_installed())
 
   # Update pyasn1
   system2(
