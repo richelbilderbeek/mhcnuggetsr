1d3d66f05c5bfc436f5027f37726581c658c1d61
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-29 11:35:16 +0200

    Install MHCnuggets by mhcnuggetsr

--- scripts/install_mhcnuggets.sh
+++ scripts/install_mhcnuggets.sh
@@ -14,7 +14,6 @@ cat R/install_mhcnuggets.R > tempscript.R
 cat R/get_default_mhcnuggets_folder.R >> tempscript.R
 cat R/get_mhcnuggets_url.R >> tempscript.R
 cat R/is_mhcnuggets_installed.R >> tempscript.R
-
 echo "install_mhcnuggets()" >> tempscript.R
 sed -i'.orginal' "s/mhcnuggetsr:://g" tempscript.R
 cat tempscript.R
