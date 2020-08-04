a053717f05c52159145743783e8a984d5531577e
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-04 10:43:40 +0200

    Do not test 'set_pip_version' on Travis

--- tests/testthat/test-set_pip_version.R
+++ tests/testthat/test-set_pip_version.R
@@ -2,20 +2,29 @@ test_that("use", {
   if (!is_on_ci()) return()
   if (!is_pip_installed()) return()
 
-  # Store the current version
-  # Assumes this is the latest upgrade
-  current_version <- get_pip_version()
+  # Only works locally
+  if (1 == 2) {
 
-  # Pick version
-  version <- "9.0.1"
-  expect_true(version != current_version)
-  set_pip_version(version)
-  expect_equal(version, get_pip_version())
+    # Store the current version
+    # Assumes this is the latest upgrade
+    current_version <- get_pip_version()
 
-  # Restore current version
-  # Note that 'set_pip_version("20.2")' does not work on Travis,
-  # as this results in a warning to upgrade pip.
-  upgrade_pip()
-  expect_equal(current_version, get_pip_version())
+    # Pick version
+    version <- "19.0"
+    expect_true(version != current_version)
+    set_pip_version(version)
+    expect_equal(version, get_pip_version())
 
+    # Pick version
+    version <- "9.0.1"
+    expect_true(version != current_version)
+    set_pip_version(version)
+    expect_equal(version, get_pip_version())
+
+    # Restore current version
+    # Note that 'set_pip_version("20.2")' does not work on Travis,
+    # as this results in a warning to upgrade pip.
+    set_pip_version(current_version)
+    expect_equal(current_version, get_pip_version())
+  }
 })
