76086ab5a86e053812a785a52ad84f6a72476c0a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-14 15:12:33 +0200

    Simplified test

--- tests/testthat/test-are_mhcnuggets_names.R
+++ tests/testthat/test-are_mhcnuggets_names.R
@@ -2,6 +2,6 @@ test_that("use", {
 
   if (!is_mhcnuggets_installed()) return()
 
-  expect_true(all(are_mhcnuggets_names(get_mhc_1_haplotypes()[1])))
-  expect_true(all(are_mhcnuggets_names(get_mhc_2_haplotypes()[1])))
+  expect_true(all(are_mhcnuggets_names(get_mhc_1_haplotypes()[1:2])))
+  expect_true(all(are_mhcnuggets_names(get_mhc_2_haplotypes()[1:2])))
 })
