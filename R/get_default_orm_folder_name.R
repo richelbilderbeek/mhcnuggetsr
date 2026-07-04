522dee2eb1e414763b413e962f49fb13b24928bf
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-07-04 17:33:50 +0200

    No refs to ormr

--- R/get_default_orm_folder_name.R
+++ R/get_default_orm_folder_name.R
@@ -1,9 +1,9 @@
-#' Get the default folder for \link[ormr]{ormr}.
+#' Get the default folder for ormr.
 #'
 #' This is either `python3` (such as in this case, when the user uses `pip`
-#' to install Python packages) or a folder where \link[ormr]{ormr} installs
+#' to install Python packages) or a folder where 'ormr' installs
 #' Python packages using \link[reticulate]{reticulate}.
-#' @return the default \link[ormr]{ormr} folder
+#' @return the default 'ormr' folder
 #' @examples
 #' get_default_orm_folder_name()
 #' @author Richèl J.C. Bilderbeek
